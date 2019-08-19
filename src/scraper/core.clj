85fc4d2bfa394673f9de180f1f47332c116ddb08
John Gargalionis <johngargalionis@gmail.com>
2019-08-20 01:22:27 +1000

    Progress on match stats

--- src/scraper/core.clj
+++ src/scraper/core.clj
@@ -4,6 +4,14 @@
             [hickory.select :as s]
             [clojure.string :as str]))
 
+(def BASE-URL "https://afltables.com/afl/")
+
+(defn read-url [url]
+  (-> (client/get url)
+      :body
+      parse
+      as-hickory))
+
 (def site-tree
   (-> (client/get "https://afltables.com/afl/seas/2018.html#lad")
       :body
@@ -43,22 +51,36 @@
           :content first
           str/trim)}))
 
-(defn parse-metadata [side]
-  (let [[_ _ _ metadata] (:content side)]
-     {:time
-      (->> metadata
-           :content first str/trim)
-      :venue
-      (-> metadata
-          :content last
-          :content first
-          str/trim)}))
+(defn get-stats-url [away]
+  (->> away
+       last last last
+       :content
+       (remove #(= "]" %))
+       last
+       :attrs :href))
+
+(defn full-path [base rel]
+  (str base
+       (last (str/split rel #"\.\./"))))
+
+(defn parse-metadata [home away]
+  (let [[_ _ _ metadata] (:content home)]
+    {:time
+     (->> metadata
+          :content first str/trim)
+     :venue
+     (-> metadata
+         :content last
+         :content first
+         str/trim)
+     :stats
+     (full-path BASE-URL (get-stats-url away))}))
 
 (defn parse-match-header [data]
   (let [[home away] (remove #(= "\n" %) data)]
     {:home (parse-side home)
      :away (parse-side away)
-     :metadata (parse-metadata home)}))
+     :metadata (parse-metadata home away)}))
 
 (defn bye-get-team [data]
   (->> data
@@ -142,11 +164,7 @@
        vec))
 
 (defn afl-data [year]
-  (let [tree (-> (client/get
-                  (str "https://afltables.com/afl/seas/"
-                       year
-                       ".html#lad"))
-                 :body parse as-hickory)
+  (let [tree (read-url (str BASE-URL "seas/" year ".html#lad"))
         data (map :content
                   (s/select
                    (s/child (s/tag :table)
@@ -154,6 +172,30 @@
                    tree))]
     {:homenaway (parse-season data)
      :finals (parse-finals data)}))
+
+(defn match-stats [match]
+  (let [stats-url (get-in match [:metadata :stats])]
+    stats-url))
+
+; TODO Remove this once above is written
+(def stats-ex
+  (get-in (first (:matches (first (:homenaway (afl-data 2018)))))
+          [:metadata :stats]))
+
+(defn match-stats [url]
+  (s/select (s/child (s/tag :tr))
+            (read-url url)))
+
+(defn parse-match-stats [data]
+  (let [ump (nth data 5)
+        key (nth data 6)
+        home-heading (nth data 7)
+        home-header (nth data 8)
+        ;; ... then the players
+        ]
+    ump))
+
+; (s/select (s/child (s/tag :th) (s/attr {:colspan "25"})) stats-ex)
 
 (defn -main
   "I don't do a whole lot."
